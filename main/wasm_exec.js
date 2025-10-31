b483e0948f59913a5522f0ccc5cbc6029326791c
Jeff Hykin <jeff.hykin@gmail.com>
2025-10-31 08:05:09 -0500

    add exec

--- main/wasm_exec.js
+++ main/wasm_exec.js
@@ -555,4 +555,11 @@ export const Go = (exports.Go = class {
     }
 })
 
+export const exec = (wasm, args) => new Promise((resolve, reject) => {
+    const go = new Go();
+    go.exit = resolve;
+    go.argv = go.argv.concat(args || []);
+    WebAssembly.instantiate(wasm, go.importObject).then((result) => go.run(result.instance)).catch(reject);
+});
+
 export default exports
